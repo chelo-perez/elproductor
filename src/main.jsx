04b51baab70fa94c1dd337539a14be6a588b5d7c
chelo-perez <marceloperezmobile@gmail.com>
2026-09-07 21:29:36 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@fontsource/anton";
+import "@fontsource/cinzel/600.css";
+import "@fontsource/cinzel/700.css";
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/400-italic.css";
+import "./index.css";
+import App from "./App.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App /></React.StrictMode>);
